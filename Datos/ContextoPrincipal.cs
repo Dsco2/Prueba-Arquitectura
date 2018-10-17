535752b608df4d4c8cf1dc470c1395f682a9a0fa
Felipe suarez <suarez.a.felipe@outlook.com>
2018-10-17 18:19:12 -0500

    ARQ: Configuración inicial del mapeo

---
+++ Datos/ContextoPrincipal.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Negocio.Productos.Entidades;
+using Persistencia.Productos.Mapas;
+
+namespace Persistencia
+{
+    public class ContextoPrincipal : DbContext
+    {
+        public DbSet<Articulo> Articulos { get; set; }
+        public ContextoPrincipal(DbContextOptions<ContextoPrincipal> options) : base(options) { }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArticuloMapa());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Datos/Productos/Mapas/ArticuloMapa.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Negocio.Productos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Productos.Mapas
+{
+    public class ArticuloMapa : IEntityTypeConfiguration<Articulo>
+    {
+        public void Configure(EntityTypeBuilder<Articulo> builder)
+        {
+            builder.ToTable(null, "articulos");
+            builder.HasKey(x => x.IdArticulo);
+        }
+    }
+}
