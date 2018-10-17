62c7f1ac47669c4dfbd5484d0673558c0838030a
Felipe suarez <suarez.a.felipe@outlook.com>
2018-10-17 18:41:52 -0500

    ARQ: Configuración parcial de Postgresql

---
+++ Datos/ContextoPrincipalFabrica.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class ContextoPrincipalFabrica : IDesignTimeDbContextFactory<ContextoPrincipal>
+    {
+        public ContextoPrincipal CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ContextoPrincipal>();
+            builder.UseNpgsql(
+            @"Host=localhost;Database=LEC-Core-LocalDb;Username=postgres;Password=admin",
+            optionsBuilder => optionsBuilder
+            .MigrationsAssembly(typeof(ContextoPrincipal)
+            .GetTypeInfo()
+            .Assembly
+            .GetName().Name)             
+                );
+            return new ContextoPrincipal(builder.Options);
+        }
+    }
+}
